9168badb99a9e50822b91a6fedaeb9cf2ff36eb7
evan-mick <evsammick@gmail.com>
2024-05-16 02:52:36 +0900

    timing variables

--- opengl_logic.go
+++ opengl_logic.go
@@ -24,18 +24,18 @@ const (
 		}
 	` + "\x00"
 
-	fragmentShaderSource = `
+	/*fragmentShaderSource = `
 		#version 410
 		uniform sampler2D textureSampler;
 		out vec4 fragColor;
 
-		uniform vec2 res; 
+		uniform vec2 res;
 
 		void main() {
 			fragColor = texture(textureSampler, vec2(gl_FragCoord.x/res.x / 2.0, 1.0 - (gl_FragCoord.y/res.y/2.0)));
 			//fragColor = vec4(float(gl_FragCoord.x)/res.x, float(gl_FragCoord.y)/res.y, 1.0, 1.0);//vec4(0.0, 0.0, 1.0, 1.0); //vec4(gl_FragCoord.x/res.x, gl_FragCoord.y/res.y, gl_FragCoord.z/500.f, 1.0);
 		}
-	` + "\x00"
+	` + "\x00"*/
 )
 
 var (
@@ -53,6 +53,8 @@ var (
 		1, 1, 0,
 		-1, 1, 0,
 	}
+
+	previousTime float64
 
 	// fragmentShaderSource = "\x00"
 )
@@ -150,6 +152,13 @@ func initGLProgram() OpenGLProgram {
 func glDraw(window *glfw.Window, program OpenGLProgram) {
 
 	gl.UseProgram(program.programID)
+
+	time := glfw.GetTime()
+	elapsed := time - previousTime
+	previousTime = time
+
+	gl.Uniform1f(gl.GetUniformLocation(program.programID, gl.Str("iTime\x00")), float32(time))
+	gl.Uniform1f(gl.GetUniformLocation(program.programID, gl.Str("deltaTime\x00")), float32(elapsed))
 
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	//glSetShaderData(program)
